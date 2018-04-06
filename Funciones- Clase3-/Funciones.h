7db927466fa952578932f83b98d262e2d1e85469
marianoyama <36230641+marianoyama@users.noreply.github.com>
2018-04-06 11:54:13 -0300

    Add files via upload

---
+++ Funciones- Clase3-/Funciones.h
@@ -0,0 +1,11 @@
+
+/** \brief
+ *
+ * \param mensaje char*  (Mensaje a ser mostrado)
+ * \param mensajeError char* (Mensaje a ser mostrado)
+ * \param maximo int (Valor maximo del intervalo)
+ * \param minimo int (Valor minimo del intervalo)
+ * \return int (Retorna un numero validado)
+ *
+ */
+int getEntero(char* mensaje,char* mensajeError, int maximo, int minimo);
---
+++ Funciones- Clase3-/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Funciones.h"
+
+
+ int main()
+{
+    int numero;
+    numero = getEntero("Ingrese su edad", "Reingrese su edad del 1 al 199", 199,1);
+    printf = ("Su edad ingresada es : %d", numero);int getEntero(char* mensaje, int maximo, int minimo);
+
+
+
+}
+
+
+
