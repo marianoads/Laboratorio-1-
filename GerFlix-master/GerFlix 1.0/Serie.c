24b91cd52e93106476cca7846c03c3cab6a7f59b
marianoyama <36230641+marianoyama@users.noreply.github.com>
2018-04-27 10:48:42 -0300

    Add files via upload

---
+++ GerFlix-master/GerFlix 1.0/Serie.c
@@ -0,0 +1,66 @@
+
+#include "Serie.h"
+#include <string.h>
+#include <stdio.h>
+
+
+/*
+    int idSerie;
+    char nombre[50];
+    char genero[20];
+    int cantidadTemporadas;
+    int estado;
+
+*/
+
+void inicializarSeriesEstado(eSerie series[], int cant)
+{
+    int i;
+
+    for(i=0; i<cant; i++)
+    {
+        series[i].estado = 0;
+    }
+}
+
+
+void inicializarSeriesHardCode(eSerie series[])
+{
+    int id[5] = {100,101,102,103,104};
+    char nombre[][50]={"TBBT","TWD","GOT","BB","LCDP"};
+    char genero[][50]={"Comedia","Terror","Suspenso","Policial","Policial"};
+    int cantidad[5] = {10,9,7,5,2};
+
+    int i;
+
+    for(i=0; i<5; i++)
+    {
+        series[i].idSerie=id[i];
+        series[i].cantidadTemporadas=cantidad[i];
+        series[i].estado = 1;
+        strcpy(series[i].nombre, nombre[i]);
+        strcpy(series[i].genero, genero[i]);
+    }
+
+
+}
+void mostrarListaSeries(eSerie serie[], int tam)
+{
+    int i;
+
+        for(i=0; i<tam;i++ )
+        {
+
+        if(serie[i].estado == 1)
+
+        {
+        printf("%d %s %s %d\n", serie[i].idSerie, serie[i].nombre, serie[i].genero, serie[i].cantidadTemporadas);
+        }
+
+        }
+
+}
+
+
+
+
---
+++ GerFlix-master/GerFlix 1.0/Usuario.c
@@ -0,0 +1,54 @@
+
+#include "Usuario.h"
+#include <stdio.h>
+#include <string.h>
+void inicializarUsuariosEstado(eUsuario usuarios[], int cant)
+{
+    int i;
+
+    for(i=0; i<cant; i++)
+    {
+        usuarios[i].estado = 0;
+    }
+}
+void inicializarUsuariosHardCode(eUsuario usuarios[])
+{
+
+
+    int id[15] = {1000,1001,1002,1003,1004, 1005,1006,1007,1008,1009, 1010,1011,1012,1013,1014};
+    char nombre[][50]= {"Juan","Maria","Pedro","Vanesa","Jose","Luisa","Laura","Marcelo","German","Victoria","Dafne","Antonela","Gisele","Dario","Pedro"};
+
+    int serie[15] = {100,100,101,101,102,100,100,103,101,102,103,101,100,100,101};
+
+
+
+    int i;
+
+    for(i=0; i<15; i++)
+    {
+        usuarios[i].idUsuario=id[i];
+        usuarios[i].idSerie=serie[i];
+        usuarios[i].estado = 1;
+        strcpy(usuarios[i].nombre, nombre[i]);
+
+    }
+void mostrarListaUsuarios(eUsuario usuarios[], int tam)
+{
+    int i;
+    for(i=0;i<tam;i++)
+    {
+        if(usuarios[i].estado==1)
+        {
+            printf("%d %s %d", usuarios[i].idUsuario, usuarios[i].nombre, usuarios[i].idSerie);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+}
